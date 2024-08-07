26be9beff3275e32ff13aa2443e6421b464df1ed
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-07 19:19:54 -0400

    update global container + words

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,15 @@ const Header = () => {
   return (
     <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">hidden mobile + block </p>
+        <p className="text-white/100 hidden md:block">✨ Hi there! </p>
         <div className="inline-flex gap-1 items-center">
-          <p></p>
+          <p>✨ Thanks for visiting my site! ✨</p>
         </div>
       </div>
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
-            <p>Header</p>
+            <p>Carlos Villanueva</p>
             <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
               <a href="#">About</a>
