aa910d54b234beb7004fc9002caa36314133bed7
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-07 19:10:48 -0400

    update to have svg functinality for images

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MenuIcon from "@/public/assets/menu.svg";
 
 const Header = () => {
   return (
@@ -6,20 +7,21 @@ const Header = () => {
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">hidden mobile + block </p>
         <div className="inline-flex gap-1 items-center">
-          <p>mobile view</p>
+          <p></p>
         </div>
       </div>
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
-            {/* <MenuIcon className="h-5 w-5 md:hidden" /> */}
+            <p>Header</p>
+            <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
               <a href="#">About</a>
               <a href="#">Projects</a>
               <a href="#">Resume</a>
               {/* <a href="#">Other</a> */}
               {/* <a href="#">Other</a> */}
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">Get for free</button>
+              <a href='#'><button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">Lets Connect</button></a>
             </nav>
           </div>
         </div>
