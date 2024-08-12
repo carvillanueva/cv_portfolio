1fcdc4be30d5afd1691dfce7079eacfaaf155386
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-12 14:34:40 -0400

    create mobile menu screen

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,7 @@ const Footer = () => {
     <footer className="bg-black text-[#BCBCBC] text-md py-10 text-center">
       <div className="container">
         <div className="inline-flex relative before:content-['']  before:h-full before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
-          {/* <Image src={logo} alt="SaaS Logo" height={40} className="relative" /> */}
-          <p className="relative z-20 text-black font-semibold">This is the end! 👋</p>
+          <p className="relative z-5 text-black font-semibold">This is the end! 👋</p>
         </div>
         <p className="mt-6">&copy; 2024 CV Labs, Inc. All rights reserved.</p>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import MenuIcon from "@/public/assets/menu.svg";
 import CloseIcon from "@/public/assets/close.svg";
+import SocialLinkedIn from "@/public/assets/social-linkedin.svg";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,34 +20,51 @@ const Header = () => {
           <p>✨ Thanks for visiting my site! ✨</p>
         </div>
       </div>
-      <div className="py-5">
-        <div className="container">
-          <div className="flex items-center justify-end">
-            <p className="hidden">Carlos Villanueva</p>
-            {isMenuOpen ? (
-              <>
-                <CloseIcon className="h-5 w-5 md:hidden" onClick={toggleMenu} />
-              </>
-            ) : (
-              <>
-                <MenuIcon className="h-6 w-6 md:hidden" onClick={toggleMenu} />
-              </>
-            )}
-            <nav className="hidden md:flex gap-6 text-black/70 items-center">
-              <a href="#">About</a>
-              <a href="#">Projects</a>
-              <a href="#">Resume</a>
-              {/* <a href="#">Other</a> */}
-              {/* <a href="#">Other</a> */}
-              <a href="#">
-                <button className="bg-[#004225] text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
-                  Lets Connect
-                </button>
-              </a>
-            </nav>
+      {isMenuOpen ? (
+        <section className="fixed inset-0 z-20 bg-white h-[100vh] overflow-hidden ">
+          <div className="p-5 mt-10">
+            <div className="flex justify-end">
+              <CloseIcon className="h-5 w-5 md:hidden" onClick={toggleMenu} />
+            </div>
+            <div className="">
+              <nav className="flex flex-col gap-6 text-black/70 items-center mt-5 ">
+                <a href="#" className="hover:underline">About</a>
+                <a href="#" className="hover:underline">Projects</a>
+                <a href="#" className="hover:underline">Resume</a>
+                {/* <a href="#">Other</a> */}
+                {/* <a href="#">Other</a> */}
+                <a href="#">
+                  <button className="bg-[#004225] text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
+                    Lets Connect
+                  </button>
+                </a>
+              </nav>
+            </div>
           </div>
-        </div>
-      </div>
+        </section>
+      ) : (
+        <section className="py-5">
+          <div className="container">
+            {/* <div className="flex items-center justify-between"> */}
+            <div className="flex items-center justify-end">
+              {/* <SocialLinkedIn className="h-7 w-7"/> */}
+              <MenuIcon className="h-6 w-6 md:hidden" onClick={toggleMenu} />
+              <nav className="hidden md:flex gap-6 text-black/70 items-center">
+                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#">Resume</a>
+                {/* <a href="#">Other</a> */}
+                {/* <a href="#">Other</a> */}
+                <a href="#">
+                  <button className="bg-[#004225] text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
+                    Lets Connect
+                  </button>
+                </a>
+              </nav>
+            </div>
+          </div>
+        </section>
+      )}
     </header>
   );
 };
