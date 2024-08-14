c5ef0ecdc0f60853ec6e636640d2dd3fe39c8f38
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-13 21:09:13 -0400

    fine tune about section

--- components/About.tsx
+++ components/About.tsx
@@ -4,9 +4,8 @@ import SchoolIcon from "@/public/assets/school.svg";
 
 const About = () => {
   return (
-    <section className="container mt-10">
-      <p className="text-3xl md:text-4xl font-semibold">About...</p>
-      <span className=""></span>
+    <section className="container mt-10" id="about">
+      {/* <p className="text-3xl md:text-4xl font-semibold">About...</p> */}
       {/* <section className="flex justify-center mt-10"> */}
       {/* <div className="text-white bg-[#004225]/60 w-[650px] m-3 p-7 rounded-lg">
         <p className="font-semibold">Little bit about me...</p>
@@ -21,72 +20,46 @@ const About = () => {
           iure facilis, quidem eaque nesciunt laborum!
         </p>
       </div> */}
-      <div className="">
-        {/* <div className="flex items-center max-w-[850px]"> */}
-        <div className="mt-5">
-          <p className="text-2xl mb-3 underline font-semibold">Experience</p>
-          <ol className="relative border-s border-gray-200 mx-3">
-            <li className="mb-10 ms-6">
-              <span className="absolute flex items-center justify-center w-6 h-6 bg-[#004225] rounded-full -start-3 ring-8 ring-white">
-                <BriefcaseIcon className="w-2.5 h-2.5 text-white" />
-              </span>
-              <h3 className="flex items-center text-xl font-semibold text-gray-900">
-                Capital One{" "}
-                <span className="bg-[#004225]/60 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded ms-3">
-                  Current
-                </span>
-              </h3>
-              <p className="mb-1">Associate Software Engineer</p>
-              <time className="block mb-2 text-sm font-normal leading-none text-gray-500">
-                August 2023 - Present
-              </time>
-              <p className="mb-4 text-base font-normal text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Facilis expedita eveniet, iste, quaerat quam quasi repellendus
-                enim odit voluptate veniam accusamus numquam quisquam.
-                Exercitationem eos vitae fugit iure quae repudiandae ad nemo cum
-                hic? Alias saepe, molestias aperiam deserunt itaque debitis
-                ratione vero dignissimos iure facilis, quidem eaque nesciunt
-                laborum!
-              </p>
-            </li>
-            <li className="ms-6">
-              <span className="absolute flex items-center justify-center w-6 h-6 bg-[#004225] rounded-full -start-3 ring-8 ring-white">
-                <BriefcaseIcon className="w-2.5 h-2.5 text-white" />
-              </span>
-              <h3 className="text-xl font-semibold text-gray-900">
-                Arlignton Family Office
-              </h3>
-              <p className="mb-1">Application Developer</p>
-              <time className="block mb-2 text-sm font-normal leading-none text-gray-500">
-                August 2021 - August 2023
-              </time>
-              <p className="text-base font-normal text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Facilis expedita eveniet, iste, quaerat quam quasi repellendus
-                enim odit voluptate veniam accusamus numquam quisquam.
-                Exercitationem eos vitae fugit iure quae repudiandae ad nemo cum
-                hic? Alias saepe, molestias aperiam deserunt itaque debitis
-                ratione vero dignissimos iure facilis, quidem eaque nesciunt
-                laborum!
-              </p>
-            </li>
-          </ol>
-        </div>
-        <div className="mt-5">
-          <p className="text-2xl mb-3 underline font-semibold">Education</p>
-          <div className="">
+      <div className="flex justify-center">
+        <div className="max-w-[850px]">
+          {/* <div className="flex items-center max-w-[850px]"> */}
+          <div className="mt-5">
+            <p className="text-2xl mb-3 underline font-semibold">Experience</p>
             <ol className="relative border-s border-gray-200 mx-3">
               <li className="mb-10 ms-6">
                 <span className="absolute flex items-center justify-center w-6 h-6 bg-[#004225] rounded-full -start-3 ring-8 ring-white">
-                  <SchoolIcon className="w-4 h-4 text-white" />
+                  <BriefcaseIcon className="w-2.5 h-2.5 text-white" />
                 </span>
                 <h3 className="flex items-center text-xl font-semibold text-gray-900">
-                  Mississippi State University{" "}
+                  Capital One{" "}
+                  <span className="bg-[#004225]/60 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded ms-3">
+                    Current
+                  </span>
                 </h3>
-                <p className="mb-1">Bachelor of Science in Biochemistry</p>
+                <p className="mb-1">Associate Software Engineer</p>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-500">
-                  August 2020
+                  August 2023 - Present
+                </time>
+                <p className="mb-4 text-base font-normal text-gray-500">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis expedita eveniet, iste, quaerat quam quasi repellendus
+                  enim odit voluptate veniam accusamus numquam quisquam.
+                  Exercitationem eos vitae fugit iure quae repudiandae ad nemo
+                  cum hic? Alias saepe, molestias aperiam deserunt itaque
+                  debitis ratione vero dignissimos iure facilis, quidem eaque
+                  nesciunt laborum!
+                </p>
+              </li>
+              <li className="ms-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-[#004225] rounded-full -start-3 ring-8 ring-white">
+                  <BriefcaseIcon className="w-2.5 h-2.5 text-white" />
+                </span>
+                <h3 className="text-xl font-semibold text-gray-900">
+                  Arlignton Family Office
+                </h3>
+                <p className="mb-1">Application Developer</p>
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-500">
+                  August 2021 - August 2023
                 </time>
                 <p className="mb-4 text-base font-normal text-gray-500">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -99,6 +72,34 @@ const About = () => {
                 </p>
               </li>
             </ol>
+          </div>
+          <div className="mt-5">
+            <p className="text-2xl mb-3 underline font-semibold">Education</p>
+            <div className="">
+              <ol className="relative border-s border-gray-200 mx-3">
+                <li className="mb-10 ms-6">
+                  <span className="absolute flex items-center justify-center w-6 h-6 bg-[#004225] rounded-full -start-3 ring-8 ring-white">
+                    <SchoolIcon className="w-4 h-4 text-white" />
+                  </span>
+                  <h3 className="flex items-center text-xl font-semibold text-gray-900">
+                    Mississippi State University{" "}
+                  </h3>
+                  <p className="mb-1">Bachelor of Science in Biochemistry</p>
+                  <time className="block mb-2 text-sm font-normal leading-none text-gray-500">
+                    August 2020
+                  </time>
+                  <p className="mb-4 text-base font-normal text-gray-500">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Facilis expedita eveniet, iste, quaerat quam quasi
+                    repellendus enim odit voluptate veniam accusamus numquam
+                    quisquam. Exercitationem eos vitae fugit iure quae
+                    repudiandae ad nemo cum hic? Alias saepe, molestias aperiam
+                    deserunt itaque debitis ratione vero dignissimos iure
+                    facilis, quidem eaque nesciunt laborum!
+                  </p>
+                </li>
+              </ol>
+            </div>
           </div>
         </div>
       </div>
