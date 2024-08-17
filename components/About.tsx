42c5452c920df8c07355555a4d6bbd02bb35f8a6
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-17 16:46:08 -0400

    update about

--- components/About.tsx
+++ components/About.tsx
@@ -3,9 +3,42 @@ import React from "react";
 const About = () => {
   return (
     <section className="container pt-[130px] pb-10" id="about">
-        <p className="text-3xl md:text-4xl font-semibold mb-3">little about me...</p>
-      <div className="">
+      <p className="text-3xl md:text-4xl font-semibold mb-3">
+        A little about me...
+      </p>
 
+      <div className="flex justify-center ">
+        <div className="max-w-[850px]">
+          <div className="mb-3">
+            <p className="">
+              I graduated from Mississippi State Univeristy with a degree in
+              Biochemistry. I had goals of going to dental school but those
+              changed when graduating during covid. During a little free time I
+              decided to learn a few things about software development. Since
+              then I have continued to grow my skills and was able to land a
+              role
+            </p>
+          </div>
+          <div className="mb-3">
+            <p className="text-lg font-semibold">What am I doing next?</p>
+            <p className="">
+              Currently I am taking time to learn NextJS and a plan to purse a MS
+              in Computer Science
+            </p>
+          </div>
+          <div className="mb-3">
+            <p className="text-lg font-semibold">
+              Since AI is a popular buzz word here are some of my favorite apps
+              to use
+            </p>
+            <div className="">
+              <ol>
+                <li>Perplexity AI</li>
+                <li>Audio Pen AI</li>
+              </ol>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,8 +26,9 @@ const Hero = () => {
         <div className="mt-8 text-center text-lg ">
           a <span className="font-semibold">Full-Stack Software Engineer</span>{" "}
           in <span className="font-semibold">Washington, D.C.</span> working for{" "}
-          <span className="font-semibold">Capital One</span>.<br></br>I
-          like building web applications and websites.
+          <span className="font-semibold">Capital One</span>.
+          <br></br>
+          {/* I like building web applications and websites. */}
         </div>
         <div className="mt-12">
           <a href="#projects">
