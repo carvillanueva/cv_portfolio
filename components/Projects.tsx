89b285e0e27f0892773b8a92f8b5a1ee13f87fe6
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-12-20 18:02:36 -0500

    adding github activity

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { projects } from "@/data/projects";
 import ProjectCard from "./ui/project-card";
+import GitHubCalendar from "react-github-calendar";
+import Link from "next/link";
 
 const Projects = () => {
   return (
     <section className="container pt-[130px] pb-10" id="projects">
-      <p className="xl:mx-20 text-3xl md:text-4xl font-semibold mb-3">Projects</p>
+      <p className="xl:mx-20 text-3xl md:text-4xl font-semibold mb-3">
+        Projects
+      </p>
       <div className="flex flex-wrap justify-center gap-6 mt-6 overflow-x-hidden max-w-full">
         {projects.map((project, index) => (
           <ProjectCard
@@ -18,6 +22,11 @@ const Projects = () => {
             projectTech={project.projectTech}
           />
         ))}
+      </div>
+      <div className="mt-10 mx-auto flex justify-center">
+        <Link href="https://github.com/carvillanueva">
+          <GitHubCalendar username="carvillanueva" />
+        </Link>
       </div>
     </section>
   );
