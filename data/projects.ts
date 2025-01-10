298ebc116b5581254aa37f9e52c4600a5cd651f8
Carlos Villanueva <carlos2villanueva@gmail.com>
2025-01-10 17:29:27 -0500

    udpdate projects list

--- data/projects.ts
+++ data/projects.ts
@@ -1,23 +1,39 @@
 export const projects = [
   {
-    title: "Wedding Website",
-    description: "Designed and hosted personal wedding website. Site contains multi page layout with RSVP form, photo gallery, and event details.",
-    projectURL: "https://www.sydneyandcarlos.com",
+    title: "Trash Management System",
+    description:
+      "Developed a trash management system to track and manage trash disposal. System allows users to be SMS notified of upcoming schedules and view history.",
+    projectURL: "https://villanueva_trash-system.vercel.app/",
     ghURL: null,
-    projectTech: ["Wordpress", "Elementor", "Google Analytics"],
-    projectImg: "/assets/sydandcar.png",
+    projectTech: [
+      "NextJS ",
+      "Postgres",
+      "Typescript",
+      "Twilio",
+      "ShadcnUI",
+      "Vercel Cron",
+    ],
+    projectImg: "/assets/villanueva-trash.png",
   },
   {
     title: "BeautyLashesByGabby",
-    description: "Designed and hosted a small business salon site. Site contains multi page layout with service details, booking form, and contact information.",
+    description:
+      "Designed and hosted a small business salon site. Site contains multi page layout with service details, booking form, and contact information.",
     projectURL: "https://www.beautylashesbygabby.com",
     ghURL: "https://github.com/carvillanueva/beautylashesbygabby",
-    projectTech: ["Angular", "Boostrap", "JavaScript", "TypeScript", "Google Analytics"],
+    projectTech: [
+      "Angular",
+      "Boostrap",
+      "JavaScript",
+      "TypeScript",
+      "Google Analytics",
+    ],
     projectImg: "/assets/beautylashes.png",
   },
   {
     title: "Python Web Scraper",
-    description: "Created a Python web scraper to extract content data from a website and have chatbot to answer questions regarding the content.",
+    description:
+      "Created a Python web scraper to extract content data from a website and have chatbot to answer questions regarding the content.",
     projectURL: null,
     ghURL: "https://github.com/carvillanueva/web_scrapper",
     projectTech: ["Python", "Selenium", "Langchain-ollama"],
@@ -25,26 +41,44 @@ export const projects = [
   },
   {
     title: "CV Portfolio Site",
-    description: "Personal portfolio. Created on NextJs. Went for a design first approach.",
+    description:
+      "Personal portfolio. Created on NextJs. Went for a design first approach.",
     projectURL: "https://carlos-villanueva.vercel.app/",
     ghURL: "https://github.com/carvillanueva/cv_portfolio",
-    projectTech: ["NextJs", "Tailwind", "Shadcn", "Vercel", "JS/TS"],
+    projectTech: ["NextJs", "Tailwind", "ShadcnUI", "Vercel", "JS/TS"],
     projectImg: "/assets/portfolio.png",
   },
   {
+    title: "Wedding Website",
+    description:
+      "Designed and hosted personal wedding website. Site contains multi page layout with RSVP form, photo gallery, and event details.",
+    projectURL: "https://www.sydneyandcarlos.com",
+    ghURL: null,
+    projectTech: ["Wordpress", "Elementor", "Google Analytics"],
+    projectImg: "/assets/sydandcar.png",
+  },
+  {
     title: "Mock Animation Site",
-    description: "Experimenting with Aceternity UI framework to create a mock website. Deployed on Vercel to test new deployment style.",
+    description:
+      "Experimenting with Aceternity UI framework to create a mock website. Deployed on Vercel to test new deployment style.",
     projectURL: "https://animation-site-alpha.vercel.app/",
     ghURL: "https://github.com/carvillanueva/animationSite",
-    projectTech: ["NextJs", "Tailwind", "Aceternity UI", "Vercel", "Framer Motion"],
+    projectTech: [
+      "NextJs",
+      "Tailwind",
+      "Aceternity UI",
+      "Vercel",
+      "Framer Motion",
+    ],
     projectImg: "/assets/animationSite.png",
   },
   {
     title: "Mock SaaS Site",
-    description: "Experimenting with Framer Motion UI styles to create mock SaaS website. Deployed on Vercel to test new deployment style.",
+    description:
+      "Experimenting with Framer Motion UI styles to create mock SaaS website. Deployed on Vercel to test new deployment style.",
     projectURL: "https://website-pi-wheat.vercel.app/",
     ghURL: "https://github.com/carvillanueva/randomSite",
-    projectTech: ["NextJs", "Tailwind", "Shadcn", "Vercel", "Framer Motion"],
+    projectTech: ["NextJs", "Tailwind", "ShadcnUI", "Vercel", "Framer Motion"],
     projectImg: "/assets/mockSaas.png",
   },
 ];
