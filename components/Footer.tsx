315e716ac4deaee079208763179265f9fb250fa2
Carlos Villanueva <carlos2villanueva@gmail.com>
2025-02-06 20:07:38 -0500

    update footer year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="inline-flex relative before:content-['']  before:h-full before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
           <p className="relative z-5 text-black font-semibold">This is the end! Bye-Bye 👋</p>
         </div>
-        <p className="mt-6">&copy; 2024 Carlos Villanueva, Inc. All rights reserved.</p>
+        <p className="mt-6">&copy; 2025 Carlos Villanueva, Inc. All rights reserved.</p>
       </div>
     </footer>
   );
